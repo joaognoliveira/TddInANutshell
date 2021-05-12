972beac06f6868aaaddbfca5a0f95eb2b46033b1
joaoGabriel <j199617@dac.unicamp.br>
2021-05-12 01:11:54 -0300

    Changing Jankenpon to cover more outcomes. to do Refactor

--- Jankenpon.java
+++ Jankenpon.java
@@ -1,6 +1,29 @@
 public class Jankenpon {
 
     public String match(String player, String adversary){
-        return "Paper";
+        if (player== "Rock" && adversary == "Paper"){
+            return "Paper";
+        }
+        else if (player== "Rock" && adversary == "Scissors"){
+            return "Rock";
+        }
+        else if (player== "Paper" && adversary == "Scissors"){
+            return "Scissors";
+        }
+        else if (player== "Paper" && adversary == "Rock"){
+            return "Paper";
+        }
+        else if (player== "Scissors" && adversary == "Paper"){
+            return "Scissors";
+        }
+        else if (player== "Scissors" && adversary == "Rock"){
+            return "Rock";
+        }
+        else if (player == adversary){
+            return "Draw";
+        }
+        else {
+            return "Error";
+        }
     }
 }
