a682b42fbbc982b937b9e15f9c29c630fc14acf8
joaoGabriel <j199617@dac.unicamp.br>
2021-05-12 01:15:45 -0300

    Adding more tests to bullet proof our Jankenpon

--- JankenponTest.java
+++ JankenponTest.java
@@ -5,7 +5,7 @@ import org.junit.Assert;
 public class JankenponTest {
 
     @Test
-    public void testMatch(){
+    public void testRockPaperMatch(){
         Jankenpon jankenpon = new Jankenpon();
 
         String player = "Rock", adversary = "Paper";
@@ -14,12 +14,57 @@ public class JankenponTest {
     }
 
     @Test
-    public void testDraw(){
+    public void testRockRockMatch(){
         Jankenpon jankenpon = new Jankenpon();
 
         String player = "Rock", adversary = "Rock";
         String matchOutcome = jankenpon.match(player, adversary);
         Assert.assertEquals("Draw", matchOutcome);
+    }
+
+    @Test
+    public void testRockScissorsMatch(){
+        Jankenpon jankenpon = new Jankenpon();
+
+        String player = "Rock", adversary = "Scissors";
+        String matchOutcome = jankenpon.match(player, adversary);
+        Assert.assertEquals("Rock", matchOutcome);
+    }
+
+    @Test
+    public void testPaperPaperMatch(){
+        Jankenpon jankenpon = new Jankenpon();
+
+        String player = "Paper", adversary = "Paper";
+        String matchOutcome = jankenpon.match(player, adversary);
+        Assert.assertEquals("Draw", matchOutcome);
+    }
+
+    @Test
+    public void testScissorsPaperMatch(){
+        Jankenpon jankenpon = new Jankenpon();
+
+        String player = "Scissors", adversary = "Paper";
+        String matchOutcome = jankenpon.match(player, adversary);
+        Assert.assertEquals("Scissors", matchOutcome);
+    }
+
+    @Test
+    public void testScissorsScissorsMatch(){
+        Jankenpon jankenpon = new Jankenpon();
+
+        String player = "Scissors", adversary = "Scissors";
+        String matchOutcome = jankenpon.match(player, adversary);
+        Assert.assertEquals("Draw", matchOutcome);
+    }
+
+    @Test
+    public void testPaperRockMatch(){
+        Jankenpon jankenpon = new Jankenpon();
+
+        String player = "Paper", adversary = "Rock";
+        String matchOutcome = jankenpon.match(player, adversary);
+        Assert.assertEquals("Paper", matchOutcome);
     }
 
 }
